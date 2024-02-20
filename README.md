e7fe1eb25bd21ca2976e0c9f1c0f82c93fef9196
DerKleinePunk <roboter@mine-robo.de>
2024-02-20 10:48:11 +0100

    Do not Build

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # This is an Test Projekt to Host an Dart VM and Run User Code in the C# Webservice
+
+## Building
+please read README.md from Dart Dynamic Import Library Auto Build Current not Work
+CMake say lib not found wenn SDK ist not build
+Im search for Solution without command on Console from user...
--- src/DartVMHelper/DartVMHelper.cpp
+++ src/DartVMHelper/DartVMHelper.cpp
@@ -1,33 +1,19 @@
 #include "include/DartVMHelper.h"
+#include <dart_dll.h>
 #include <dart_api.h>
 #include <iostream>
 
 int InitSystem(char* param1)
 {
     std::cout << param1 << " Starting Dart" << std::endl;
-
-    auto buffer_ = reinterpret_cast<char*>(::malloc(64));
-
-    free(buffer_);
-    buffer_ = nullptr;
-
     std::cout << "Version " << Dart_VersionString() << std::endl;
 
-    const char* where = "C:\\Develop\\flutter\\bin\\cache\\dart-sdk\\bin";
-    auto result = Dart_SetVMFlags(1, &where); // Command Line
-    if(result != nullptr) {
-        std::cout << result << std::endl;
+    // Initialize Dart
+    DartDllConfig config;
+    config.service_port = 6222;
+
+    if (!DartDll_Initialize(config)) {
         return -1;
-    }
-
-    Dart_InitializeParams initParams;
-    memset(&initParams, 0, sizeof(initParams));
-    initParams.version = DART_INITIALIZE_PARAMS_CURRENT_VERSION;
-
-    result = Dart_Initialize(&initParams);
-    if(result != nullptr) {
-        std::cout << result << std::endl;
-        return -2;
     }
 
     std::cout << param1 << "Dart Started" << std::endl;
